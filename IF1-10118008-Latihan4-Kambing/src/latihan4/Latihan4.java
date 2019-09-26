858197c842083cb73765b56fc87412772c19c8cd
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-09-26 18:34:26 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan4-Kambing/src/latihan4/Latihan4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4;
+
+/**
+ *
+ *  @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF1
+ * NIM			: 10118008
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata rockstar
+ */
+public class Latihan4 {
+
+	public void tambahKambing(){
+		int jumlahKambing = 0;
+		jumlahKambing = jumlahKambing + 5;
+		System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+	}
+	public static void main(String[] args) {
+		Latihan4 kambingJantan = new Latihan4();
+		kambingJantan.tambahKambing();
+	}
+	
+}
